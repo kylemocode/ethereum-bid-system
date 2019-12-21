3e093c30fd350c3ac5b151f9e7984dd3e308f05c
ALLEN-CHUANG <n08041134@gmail.com>
2019-12-21 15:29:51 +0800

    --yuneng add contract and index.js

--- README.md
+++ README.md
@@ -1,52 +1,68 @@
 #  盲拍投標板
-##   流程
+
+##  Install 
+    Pleas first install by using npm
+    ```
+    $ git clone https://github.com/ALLEN-CHUANG/eth_final_project.git
+    $ cd eth_final_project; npm insatll
+    $ npm install -g ganache-cli
+    ```
+## Activate 
+    First make sure you have activate Ganache CLI
+    ```
+    $ ganache-cli --blockTime 5
+    ```
+
+## Some function bottoms required (Need input and output declare)
+    Rough progress is in main function.
+### for user 
+    (0) contract deploy (func: build {input: (private_key, bid_endtime, reveal_endtime, what_account})
+    (1) bid ( func: bid )
+    (2) reveal auction ( func: reveal )
+    (3) withdraw ( func: withdraw )
+    (4) auction end ( func: auctionEnd )
+    (5) get highest bid price ( func: highbid )
+    (6) who is highbider ( func: highbider )
+    (7) get account balance
+    (8) Account login 
+
+##  流程
     user進行deploy contract
     (0) user抵押1000
     (1) user寫投標板                                        (流程鎖#1)
     (2) user進行reveal                                     (流程鎖#2)(時間鎖"before(revealEnd)")
     (3) user決定是否withdraw                                (流程鎖#3)
     若withdraw,則將當下的抵押餘額存入"pocket",並回到(0)
-    (4) auctionend                                                  (時間鎖"after(revealEnd)")
+    (4) auctionend                                         (時間鎖"after(revealEnd)")
     (5) 根據用戶之"pocket"中的金額進行還款       
-##  函式
+
+##  Function Description in blind.sol (Need more detail descrip.)
 ### bid
-        若用戶狀態為"0"(尚未寫板),紀錄盲標,並更改用戶狀態為"1"(已寫板)
+        用戶投標專用，用戶使用此 fucntion 將欲投標之金額上標。
+        ( 若用戶狀態為"0"(尚未寫板),紀錄盲標,並更改用戶狀態為"1"(已寫板) )
 ### reveal 
         (0) 在"revealEnd"之前皆可用
-        
-        (1)
-            判斷用戶輸入的資料是否與先前寫板時的盲標一致,若沒有不一致,進入(2),否則進入(5)
-        (2)
-            將refund 設為 deposits[msg.sender](用戶抵押金),若 refund >= 出價 ,進入(3)
-        (3)
-            判斷是否為當前最高出價,若是,進入(3-1);若否,則進入(3-2)
-        (3-1) refund 扣除 (出價＋訂金+手續費),進入(4)
-        (3-2) refund 扣除 (出價＋訂金+手續費),並將(出價＋訂金)加入pendingReturn,進入(4)
-        (4)
-            清除盲標資料(防止重複reveal),進入(5)
-        (5)
-            將refund加入pendingReturn,將用戶狀態設為"2"(已出價)
-### placebid
-    若出價最高,更新"最高出價者"(highestBidder)與"最高標金"(highestBid)
-
+        (1) 判斷用戶輸入的資料是否與先前寫板時的盲標一致,若沒有不一致,進入(2),否則進入(5)
+        (2) 將refund 設為 deposits[msg.sender](用戶抵押金),若 refund >= 出價 ,進入(3)
+        (3) 判斷是否為當前最高出價,若是,進入(3-1);若否,則進入(3-2)
+            (3-1) refund 扣除 (出價＋訂金+手續費),進入(4)
+            (3-2) refund 扣除 (出價＋訂金+手續費),並將(出價＋訂金)加入pendingReturn,進入(4)
+        (4) 清除盲標資料(防止重複reveal),進入(5)
+        (5) 將refund加入pendingReturn,將用戶狀態設為"2"(已出價)
 ### withdraw
-        (1)
-            若用戶之pendingReturn不為0,且用戶狀態為"2"(已出價),進入(2)
-        (2)
-            將pendingReturn的值加入"總還款金額"(pocket[msg.sender]),並將用戶狀態設為"0"(尚未寫板)
-    p.s.用戶可透過是否能重新可bid去觀察自己是否成功placeBid
+        (1) 若用戶之pendingReturn不為0,且用戶狀態為"2"(已出價),進入(2)
+        (2) 將pendingReturn的值加入"總還款金額"(pocket  [msg.sender]),並將用戶狀態設為"0"(尚未寫板)
+        p.s.用戶可透過是否能重新可bid去觀察自己是否成功placeBid
 ### auctionEnd
         (0) 在"revealEnd"之後皆可用
-            
-        (1)
-            宣告結束(ended = true),更新payvalue
+        (1) 宣告結束(ended = true),更新payvalue
+
 ### checkprice
             查看highestBid
 ### checkhb
             查看highestBidder
 ### checkpocket
             (0)在"revealEnd"之後皆可用
-        
             查看"用戶的總還款金額"(pocket[msg.sender])          
 
         
---
+++ index.js
@@ -0,0 +1,208 @@
+const Web3 = require('web3');
+const solc = require('solc');
+const fs = require('fs');
+const TX = require('ethereumjs-tx').Transaction;
+
+
+// // Connect to local node
+const web3 = new Web3('http://localhost:8545');
+var contracts_path = "contract/blind_v19.sol";
+var input = fs.readFileSync(contracts_path, 'utf8');
+
+
+// 1 activates the optimiser
+var output = solc.compile(input);
+//console.log(output);
+
+const bytecode = output.contracts[':BlindAuction'].bytecode;
+const abi = JSON.parse(output.contracts[':BlindAuction'].interface);
+//(console.log(bytecode);
+//console.log(abi);
+
+
+// deploy a contract
+async function build(abi, privke, bidend, revealend, who){
+
+	let accounts = await web3.eth.getAccounts()
+
+	//const privke= "d19d68c5009a8801681f3c38f82c9d49903c98ec5d7da398f67808de383deb60";
+	const account = accounts[who]; 
+	const privkey = Buffer.from(privke, 'hex');
+
+	if (web3.eth.personal.unlockAccount(account, privke)) {
+		console.log(`${account} is unlocaked`);
+	}else{
+		console.log(`unlock failed, ${account}`);
+	}
+
+	//let gasEstimate = web3.eth.estimateGas({data: '0x' + bytecode});
+	//console.log('gasEstimate = ' + gasEstimate); //'20000000000'
+	console.log('deploying contract...');
+	//let MyContract = new web3.eth.Contract(abi, '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe', {from: account  , gasPrice: '20000000000', data: bytecode, gas: 100});
+	
+	let MyContract = new web3.eth.Contract(abi);
+    const nonce = await web3.eth.getTransactionCount(accounts[who]);
+    const txObject = {
+        nonce:    web3.utils.toHex(nonce),
+        gasLimit: web3.utils.toHex(4700000),
+        gasPrice: web3.utils.toHex(web3.utils.toWei('1', 'gwei')),
+        data: await MyContract.deploy({ data: '0x'+ bytecode, arguments: [bidend, revealend, accounts[who]] }).encodeABI()
+    }
+
+	var tx = new TX(txObject, {'chain': 'ropsten'});
+	//let privke = Buffer.from(privkey, 'hex');
+    tx.sign(privkey);
+    const serializedTX = tx.serialize();
+    const rawTX = '0x' + serializedTX.toString('hex');
+  
+    const txHash = await web3.eth.sendSignedTransaction(rawTX)
+	console.log(txHash);
+
+	return txHash.contractAddress;
+}
+
+
+
+async function bid(abi, contractAddress, value, fake, secret, i) {
+
+    let contract = new web3.eth.Contract(abi, contractAddress);
+	
+	//const {SHA3} = require('sha3')
+	//ar blindedBid = web3.utils.sha3(value, fake, secret);
+	
+	let accounts = await web3.eth.getAccounts()
+	//web3.eth.personal.unlockAccount(account, privkey)
+	contract.methods.bid(value, fake, secret).send({
+		from: accounts[i],
+		gas: 340000
+	})
+		.on('receipt', function (receipt) {
+			console.log('success bid')
+			console.log(receipt);
+			
+		})
+		.on('error', function (error) {
+			 console.log(error.toString());
+		})	
+	
+};
+
+async function reveal(abi, contractAddress, value, fake, secret, i) {
+	let contract = await new web3.eth.Contract(abi, contractAddress);
+	let accounts = await web3.eth.getAccounts()
+	if ( fake==1 ){
+		fake = 0;
+	}
+	contract.methods.reveal(value, fake, secret).send({
+		from: accounts[i],
+		gas: 340000
+	})
+		.on('receipt', function (receipt) {
+			console.log('success reveal')
+			console.log(receipt);
+			
+		})
+		.on('error', function (error) {
+			console.log('fail reveal')
+			console.log(error.toString());
+		})	
+
+};
+
+async function auctionend(abi, contractAddress, who) {
+	let contract = await new web3.eth.Contract(abi, contractAddress);
+	let accounts = await web3.eth.getAccounts()
+	
+	contract.methods.auctionEnd().send({
+		from: accounts[who],
+		gas: 340000
+	})
+		.on('receipt', function (receipt) {
+			console.log('success end')
+			console.log(receipt);
+		})
+		.on('error', function (error) {
+			console.log('fail end')
+			console.log(error.toString());
+		})	
+
+};
+
+async function withdraw(abi, contractAddress, who) {
+	let contract = await new web3.eth.Contract(abi, contractAddress);
+	let accounts = await web3.eth.getAccounts()
+	
+	contract.methods.withdraw().send({
+		from: accounts[who],
+		gas: 340000
+	})
+		.on('receipt', function (receipt) {
+			console.log('success withdraw')
+			console.log(receipt);
+			
+		})
+		.on('error', function (error) {
+			console.log('fail withdraw')
+			console.log(error.toString());
+		})	
+};
+
+// get account balance
+async function get_account_balance(who) {
+
+	let accounts = await web3.eth.getAccounts()
+	let weiBalance = await web3.eth.getBalance(accounts[who])
+	ethBalance = web3.utils.fromWei(weiBalance, 'ether')
+
+	console.log("Get Balance", ethBalance);
+};
+
+async function highbid(abi, contractAddress, who) {
+	let contract = await new web3.eth.Contract(abi, contractAddress);
+	let accounts = await web3.eth.getAccounts()
+	
+	let a = await contract.methods.checkprice().call({
+		from: accounts[who]
+	})
+	console.log("High", a);
+};
+
+
+// check highest bidder
+async function highbider(abi, contractAddress, who) {
+	let contract = await new web3.eth.Contract(abi, contractAddress);
+	let accounts = await web3.eth.getAccounts()
+	
+	let a = await contract.methods.checkhb().call({
+		from: accounts[who]
+	})
+	console.log("Highbidder", a);
+};
+
+
+// Check pocket of given account
+async function chckpokt(abi, contractAddress, who) {
+	let contract = await new web3.eth.Contract(abi, contractAddress);
+	let accounts = await web3.eth.getAccounts()
+	
+	let a = await contract.methods.checkpocket().call({
+		from: accounts[who]
+	})
+	console.log("pocket", a);
+};
+
+
+// async function main(){
+
+// 	const contract_address = await build(abi, "1e703f9bb3a226f70e455b9bea5a40990a960e286a9ee8511550d47418cca951" ,10, 10, 0);
+	
+// 	await bid(abi, contract_address, 100, 0, '0xa', 5);
+// 	await bid(abi, contract_address, 20, 0, '0xb', 6);
+
+// 	await reveal(abi, contract_address, 100, 0, '0xa', 5);
+// 	await reveal(abi, contract_address, 20, 0, '0xb', 6);
+
+
+// }
+
+// main().then(get_account_balance(5));
